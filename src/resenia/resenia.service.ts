e2a9efed74cb3de40352a1308abaf78dc356adf6
Jairo <ja.fierro@uniandes.edu.co>
2025-05-16 09:23:53 -0500

    Entrega

--- src/resenia/resenia.service.ts
+++ src/resenia/resenia.service.ts
@@ -2,11 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReseniaEntity } from './resenia.entity';
 import { Repository } from 'typeorm';
+import { BussinessError, BussinessLogicException } from 'src/shared/business-errors';
 
 @Injectable()
 export class ReseniaService {
   constructor(
     @InjectRepository(ReseniaEntity)
-    private readonly estudianteRepository: Repository<ReseniaEntity>,
+    private readonly reseniaRepository: Repository<ReseniaEntity>,
   ) {}
+
+  async agregarReseña(newResenia: ReseniaEntity): Promise<ReseniaEntity> {
+    if (newResenia.actividad === "Finalizada") {
+      throw new BussinessLogicException(
+        'El codigo debe tener 10 caracteres',
+        BussinessError.PRECONDITION_FAILED,
+      );
+    }
+
+    const resenia = this.reseniaRepository.save(resenia);
+    return resenia;
+  }
 }

a6ad87b667d34780e19066bbfe9e9a1711e034c6
Jairo <ja.fierro@uniandes.edu.co>
2025-05-18 16:34:05 -0500

    controller

--- src/actividad/actividad.controller.ts
+++ src/actividad/actividad.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { ActividadService } from './actividad.service';
 import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { ActividadDto } from './actividad.dto';
+import { ActividadEntity } from './actividad.entity';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('actividades')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class ActividadController {
   constructor(private readonly actividadService: ActividadService) {}
+
+  @Post()
+  crearActividad(@Body() actividadDTO: ActividadDto) {
+    const actividad =plainToInstance(ActividadEntity, actividadDTO);
+    return this.actividadService.crearActividad(actividad);
+  }
 }
--- src/resenia/resenia.dto.ts
+++ src/resenia/resenia.dto.ts
@@ -1,1 +1,15 @@
-export class ReseniaDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class ReseniaDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly comentario: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly calificacion: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly fecha: string;
+}
